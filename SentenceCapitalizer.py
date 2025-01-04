5bea63e5b20e297e8f93cb2078a49467fe3d8dde
Mustafa Berkay Sökmen <131465305+MustafaBerkaySokmen@users.noreply.github.com>
2025-01-04 16:36:04 +0300

    Add files via upload

---
+++ SentenceCapitalizer.py
@@ -0,0 +1,28 @@
+
+def capitalizer(sentences, punctuations, names):
+    """
+    Write your code inside this function.
+    """
+    return "change me" # replace this with the correct return value
+        
+
+def main():
+    """
+    You can test your capitalizer() via the function calls below.
+    You can add more tests if you want to, but you do not have to. 
+    You will not be graded on what is inside your main() function, but
+    make sure that it does not cause a syntax error.
+    """
+    sentences = "hello, my name is joe. what is your name? my name is john."
+    punctuations = ['.', '!', '?']
+    names = ["John", "James", "Alice", "Bob", "Joe"]
+    print(capitalizer(sentences, punctuations, names))
+    
+
+    
+################################################################ 
+"""
+DO NOT EDIT BELOW THIS LINE
+"""
+if __name__ == '__main__':
+    main()
